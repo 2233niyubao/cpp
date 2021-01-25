c47ca8ef77437d4646ae24fd8bbf638a5f38bcf4
daniel <2233niyubao@163.com>
2021-01-25 20:36:27 +0800

    code formation

--- cpp2_learning/variadic_printf.cpp
+++ cpp2_learning/variadic_printf.cpp
@@ -10,36 +10,43 @@ using namespace std;
 
 namespace nscpp
 {
-void printf(const char* s) {
-	while(*s){
-		if(*s=='%' && *(++s)!='%'){
-			throw runtime_error("invalid format string::miss arguments");
+	void printf(const char *s)
+	{
+		while (*s)
+		{
+			if (*s == '%' && *(++s) != '%')
+			{
+				throw runtime_error("invalid format string::miss arguments");
+			}
+			cout << *s++;
 		}
-		cout<<*s++;
 	}
-}
 
-template<typename T, typename... Args>
-void printf(const char* s, T value, Args... args){
-	while(*s){
-		if(*s=='%' && *(++s)!='%'){
-			cout << value;
-			printf(++s, args...);
-			return;
+	template <typename T, typename... Args>
+	void printf(const char *s, T value, Args... args)
+	{
+		while (*s)
+		{
+			if (*s == '%' && *(++s) != '%')
+			{
+				cout << value;
+				printf(++s, args...);
+				return;
+			}
+			cout << *s++;
 		}
-		cout << *s++;
+		throw logic_error("extra arguments provided to printf");
 	}
-	throw logic_error("extra arguments provided to printf");
-}
-}
+} // namespace nscpp
 
-int main(){
-	int* pi = new int;
+int main()
+{
+	int *pi = new int;
 	nscpp::printf("%d %s %p %f\n",
-			15,
-			"This is Ace",
-			pi,
-			3.141592653);
+				  15,
+				  "This is Ace",
+				  pi,
+				  3.141592653);
 	delete pi;
 	return 0;
 }
